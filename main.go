df0d688c308b6589b99ea6df40141384ce709906
usman-tahir <tahir.usman.ali94@outlook.com>
2017-01-14 12:20:30 -0500

    created a middleware example with negroni

--- main.go
+++ main.go
@@ -1,53 +1,31 @@
 package main
 
 import (
-  "fmt"
+  "log"
   "net/http"
-  "github.com/julienschmidt/httprouter"
+  "github.com/codegangsta/negroni"
 )
 
-func HomeHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-  fmt.Println(rw, "Home\n")
-}
-
-func PostsIndexHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-  fmt.Println(rw, "Posts Index\n")
-}
-
-func PostsCreateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-  fmt.Println("Posts Create\n")
-}
-
-func PostShowHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-  id := p.ByName("id")
-  fmt.Fprintln(rw, "Showing post", id, "\n")
-}
-
-func PostUpdateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-  fmt.Fprintln(rw, "Post Update\n")
-}
-
-func PostDeleteHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-  fmt.Fprintln(rw, "Post Delete\n")
-}
-
-func PostEditHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-  fmt.Fprintln(rw, "Post Edit\n")
+func MyMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+  log.Println("[Begin] Logging")
+  if r.URL.Query().Get("password") == "password" {
+    next(rw, r)
+    log.Println("Access granted.")
+  } else {
+    http.Error(rw, "Error: Access denied.", 401)
+    log.Println("Access denied.")
+  }
+  log.Println("[End] Logging")
 }
 
 func main() {
-  r := httprouter.New()
-  r.GET("/", HomeHandler)
+  // Middleware stack
+  n := negroni.New(
+    negroni.NewRecovery(),
+    negroni.HandlerFunc(MyMiddleware),
+    negroni.NewLogger(),
+    negroni.NewStatic(http.Dir("public")),
+  )
 
-  // Posts collection
-  r.GET("/posts", PostsIndexHandler)
-  r.POST("/posts", PostsCreateHandler)
-
-  // Single post handling
-  r.GET("/posts/:id", PostShowHandler)
-  r.PUT("/posts/:id", PostUpdateHandler)
-  r.GET("/posts/:id/edit", PostEditHandler)
-
-  fmt.Println("Starting server on localhost:8080")
-  http.ListenAndServe(":8080", r)
+  n.Run(":3000")
 }
