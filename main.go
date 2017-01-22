08c1fb86d16674073d18276bb2485e5b4ed340cd
usman-tahir <tahir.usman.ali94@outlook.com>
2017-01-21 20:24:05 -0500

    used a simple render package for JSON/HTML (coded example)

--- main.go
+++ main.go
@@ -1,31 +1,29 @@
 package main
 
 import (
-  "html/template"
   "net/http"
-  "path"
+  "./src/gopkg.in/unrolled/render.v1"
 )
 
-type Book struct {
-  Title, Author string
-}
-
-func ShowBooks(w http.ResponseWriter, r * http.Request) {
-  book := Book{"Building Web Apps with Go", "Jeremy Saenz"}
-
-  fp := path.Join("templates", "index.html")
-  tmpl, err := template.ParseFiles(fp)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-
-  if err := tmpl.Execute(w, book); err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-  }
-}
-
 func main() {
-  http.HandleFunc("/", ShowBooks)
-  http.ListenAndServe(":3000", nil)
+  r := render.New(render.Options{})
+  mux := http.NewServeMux()
+
+  mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+    w.Write([]byte("Welcome! Feel free to browse the other pages."))
+  })
+
+  mux.HandleFunc("/data", func(w http.ResponseWriter, req *http.Request) {
+    r.Data(w, http.StatusOK, []byte("Binary data"))
+  })
+
+  mux.HandleFunc("/json", func(w http.ResponseWriter, req *http.Request) {
+    r.JSON(w, http.StatusOK, map[string]string{"Hello": "JSON"})
+  })
+
+  mux.HandleFunc("/html", func(w http.ResponseWriter, req *http.Request) {
+    r.HTML(w, http.StatusOK, "Exxamp", nil)
+  })
+
+  http.ListenAndServe(":3000", mux)
 }
