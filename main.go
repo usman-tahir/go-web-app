408869fddc3c015b4656b1a84c5e66fe7543ec7a
usman-tahir <tahir.usman.ali94@outlook.com>
2017-01-21 20:36:15 -0500

    created a simple test for HTTP responses using net/httptest

--- main.go
+++ main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
+  "fmt"
   "net/http"
-  "./src/gopkg.in/unrolled/render.v1"
 )
 
+func HelloWorld(response http.ResponseWriter, request *http.Request) {
+  fmt.Fprint(response, "Hello World!")
+}
+
 func main() {
-  r := render.New(render.Options{})
-  mux := http.NewServeMux()
-
-  mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-    w.Write([]byte("Welcome! Feel free to browse the other pages."))
-  })
-
-  mux.HandleFunc("/data", func(w http.ResponseWriter, req *http.Request) {
-    r.Data(w, http.StatusOK, []byte("Binary data"))
-  })
-
-  mux.HandleFunc("/json", func(w http.ResponseWriter, req *http.Request) {
-    r.JSON(w, http.StatusOK, map[string]string{"Hello": "JSON"})
-  })
-
-  mux.HandleFunc("/html", func(w http.ResponseWriter, req *http.Request) {
-    r.HTML(w, http.StatusOK, "Exxamp", nil)
-  })
-
-  http.ListenAndServe(":3000", mux)
+  http.HandleFunc("/", HelloWorld)
+  http.ListenAndServe(":3000", nil)
 }
