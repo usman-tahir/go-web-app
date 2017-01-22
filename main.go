96d2bde1239ee3c35bbb6492b4b0925ad242e117
usman-tahir <tahir.usman.ali94@outlook.com>
2017-01-21 20:43:42 -0500

    created a basic end-to-end testing example

--- main.go
+++ main.go
@@ -3,13 +3,31 @@ package main
 import (
   "fmt"
   "net/http"
+  "./src/github.com/codegangsta/negroni"
+  "./src/github.com/julienschmidt/httprouter"
 )
 
-func HelloWorld(response http.ResponseWriter, request *http.Request) {
-  fmt.Fprint(response, "Hello World!")
+func HelloWorld(res http.ResponseWriter, req *http.Request, p httprouter.Params) {
+  fmt.Fprint(res, "Hello World")
+}
+
+func App() http.Handler {
+  n := negroni.Classic()
+
+  m := func(res http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+    fmt.Fprint(res, "Before...")
+    next(res, req)
+    fmt.Fprint(res, "...After")
+  }
+  n.Use(negroni.HandlerFunc(m))
+
+  r := httprouter.New()
+
+  r.GET("/", HelloWorld)
+  n.UseHandler(r)
+  return n
 }
 
 func main() {
-  http.HandleFunc("/", HelloWorld)
-  http.ListenAndServe(":3000", nil)
+  http.ListenAndServe(":3000", App())
 }
